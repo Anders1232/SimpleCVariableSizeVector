394a6c39d5f34e8842737d298b53b16b5704ad8e
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-01-20 19:50:54 -0200

    Documentado o resto das funções. Colocado a paǵina relativa ao vector.h no lugar do index.html, pois lá é o único lugar da documentação gerada onde as funções estão documentadas

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['deletevector',['DeleteVector',['../vector_8c.html#aa149f91e40e2c94f276c4b135e30317b',1,'DeleteVector(Vector *vec):&#160;vector.c'],['../vector_8h.html#aa149f91e40e2c94f276c4b135e30317b',1,'DeleteVector(Vector *vec):&#160;vector.c']]]
+  ['deletevector',['DeleteVector',['../vector_8c.html#ad31b2db87d84f6ef1d8fb8648275dac4',1,'DeleteVector(Vector *vec):&#160;vector.c'],['../vector_8h.html#aa149f91e40e2c94f276c4b135e30317b',1,'DeleteVector(Vector *vec):&#160;vector.c']]]
 ];
--- vector.h
+++ vector.h
@@ -42,15 +42,75 @@ typedef struct vector Vector;
 	\return A new vector wich each element have size elementSize.
 	\brief Create a new vector
 	
-	A new vector is created, the size of each element is elementSize.
-	The best way is to use with sizeof, like \code Vector *vec= NewVector( sizeof ( int ) ); \endcode
+	A new vector is created, the size of each element is elementSize.<br>
+	If the Vector allocation fails, NULL is returned. <br>
+	The best way is to use with sizeof, like: \code Vector *vec= NewVector( sizeof ( int ) ); \endcode
 */
 
 Vector* NewVector(unsigned long int elementSize);
 
-int VectorAppendCopy(Vector *vec, void* element);
+/*!
+	\fn int VectorAppendCopy(Vector *vec, void* element)
+	\param *vec A pointer to the vector.
+	\param *element A pointer to the element wich value will be copied to the vector.
+	\return Returns the the position of the value passed as argument, with the new element at the end of the vector.
+	\brief Inserts a new element at the end of the vector.
+	
+	Inserts a new element at the end of the vector, expanding the vector size if necessary.<br>
+	If the allocation fails, an error message is send to stderr and exit is called.<br>
+	
+	Example:
+	\code int newElementPosition= VectorAppendCopy( vec, &value ); \endcode
+	The example above inserts an element at the end of the vector and assigns to newElementPosition its position.
+*/
+int VectorAppendCopy(Vector *vec, void *element);
 
+/*!
+	\fn int VectorAllocateOne(Vector *vec)
+	\param *vec A pointer to the vector.
+	\return Returns the position in the vector allocated. May be used with VectorGetElement.
+	\brief Allocates space for at least one more element.
+	
+	Allocate space in the vector for at least one more element, expanding the vector size if necessary. The allocated position is uninitialized.<br>
+	If the allocation fails, an error message is send to stderr and exit is called.<br>
+	Its return value may be used with VectorGetElement.<br>
+	Example: \code 
+			Vector *vec= new Vector(sizeof(int));
+			*( (int*) VectorGetElement(vec, VectorAllocateOne(vec) ) = 2; 
+		\endcode
+	The example above assigns 2 to the first position of the vector. The vector holds elements of type "int".
+*/
 int VectorAllocateOne(Vector *vec);
+
+/*!
+	\fn void* VectorGetElement(Vector *vec, int position)
+	\param *vec A pointer to the vector.
+	\param position The wanted vector position
+	\return Returns a pointer to the element of the vector at the position informed.
+	\brief Gets the element at informed position.
+	
+	Its return value may be used with VectorGetElement.<br>
+	Since the pointer to the position is returned, you may assign new values to the position using the returned pointer.<br>
+	Example 1: \code 
+			Vector *vec= new Vector(sizeof(int));
+			*( (int*) VectorGetElement(vec, VectorAllocateOne(vec) ) = 2; 
+		\endcode
+	The example above assigns 2 to the first position of the vector. The vector holds elements of type "int".<br>
+	To assign values you may deference the pointer and then assign. Or use memcpy, declared at string.h.
+*/
 void* VectorGetElement(Vector *vec, int position);
+
+/*!
+	\fn Vector* DeleteVector(Vector* vec)
+	\param *vec A pointer to the vector.
+	\return NULL is returned
+	\brief Delete the vector and its elements.
+	
+	Deletes the vector and its elements, deallocating both.
+	Example : \code 
+			Vector *vec= new Vector(sizeof(int));
+			DeleteVector(vec);
+		\endcode
+*/
 Vector* DeleteVector(Vector* vec);
 #endif
